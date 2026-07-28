9c0e9260698f0a4f41606471c6d3b764d60dc534
Rtcoderpk <ranatanzeel098@gmail.com>
2026-07-28 23:37:47 +0500

    Fix next.config.js - remove outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: path.join(__dirname, '../../'),
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
